306ed54eeefaf68c15ac51a84aa2c5dc83b524ec
trody <mahdi.karimi.beiranvand2003@gmail.com>
2025-01-12 16:31:24 +0330

    fix : debufg and refactor some part of code

---
+++ main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/go-basics-project/pkg/server"
+)
+
+func main() {
+	server := server.NewHttpServer("localhost" , 9090)
+	server.HandleFunc("/hello" , func(w http.ResponseWriter , r *http.Request) {
+		w.Write([]byte("Hello World!"))
+	})
+	server.Start()
+
+	
+}
--- pkg/server/server.go
+++ pkg/server/server.go
@@ -13,15 +13,13 @@ type HttpServer struct {
 func NewHttpServer(host string , port int) *HttpServer {
 	return &HttpServer{
 		server : http.Server{
-			Adr : fmt.Sprintf("%s:%d" , host , port)
-		},
+			Addr : fmt.Sprintf("%s:%d" , host , port)},
 		mux: http.NewServeMux(),
 	}
 }
 
-
-func (s *HttpServer) handelFunction(pattern string , handler func(http.ResponseWriter , *http.Request)) {
-	s.mux.HandleFunc(pattern , handler)
+func (s *HttpServer) HandleFunc(pattern string, handler func(http.ResponseWriter, *http.Request)) {
+	s.mux.HandleFunc(pattern, handler)
 }
 
 
