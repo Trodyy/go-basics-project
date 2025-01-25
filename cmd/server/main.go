1b52439a3c68c8db74bc53b759fe43f7163461b5
trody <mahdi.karimi.beiranvand2003@gmail.com>
2025-01-25 18:13:46 +0330

    feat : Add code in main.go in cmd/server directory.

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,13 +1,23 @@
 package main
 
 import (
-	"github.com/Trodyy/go-basics-project/pkg/server"
+	"context"
 	"net/http"
+	"github.com/Trodyy/go-basics-project/internal/db"
+	"github.com/Trodyy/go-basics-project/internal/db/postgres"
 	"github.com/Trodyy/go-basics-project/pkg/config"
+	"github.com/Trodyy/go-basics-project/pkg/server"
 )
 
 func main() {
 	cnf := config.LoadConfigOrPanic()
+	pg , err := postgres.NewPostgres(cnf)
+	if err != nil {
+		panic(err)
+	}
+	if err := db.Migrate(context.Background(), pg); err != nil {
+		panic(err)
+	}
 	s := server.NewHttpServer(cnf.Server)
 	s.HandleFunc("/hello" , func(w http.ResponseWriter, r *http.Request) {
 		w.Write([]byte("Hello World!"))
